a148ff5ab9d723a7bad3646092f30cafbb44d72f
kongxuhui <18235160502@163.com>
2017-09-01 21:28:45 +0800

    讲师管理

--- public/js/config.js
+++ public/js/config.js
@@ -4,8 +4,28 @@ require.config({
         jquery : 'jquery/jquery.min',
         cookie : 'jquery-cookie/jquery.cookie',
         template : 'artTemplate/temlate-web',
-        teacherlist : '../js/teacher-list',
+        bootstrap : 'bootstrap/js/bootstrap.min',
+        datepicker : 'bootstrap-datepicker/js/bootstrap-datepicker.min',
+        language : 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
+        validate : 'validate/jquery-validate',
+        form : 'jquery-form/jquery.form',
         login : '../js/login',
-        common : '../js/common'
+        common : '../js/common',
+        index : '../js/index',
+        util : '../js/util',
+        teacheradd : '../js/teacheradd',
+        teacherlist : '../js/teacher-list'
+
+    },
+    shim : {//把非标准模块转化为标准模块
+        bootstrap : {
+            deps : ['jquery']
+        },
+        language : {
+            deps : ['jquery','datepicker']
+        },
+        validate : {
+            deps : ['jquery']
+        }
     }
 })
